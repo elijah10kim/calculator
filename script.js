26b69ee839e01cc76a1cf575dc615097598c6d1c
Elijah <elijah10kim@gmail.com>
2023-02-12 21:08:51 -0500

    created basic operation functions

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    let product = x * y
+    if (product == -0) {
+        product = 0
+    }
+    return product
+}
+
+function divide(x,y) {
+    if (x == -0) {
+        x = 0
+    }
+    let quotient = x / y
+    if (quotient == -0) {
+        return 0
+    }
+    return quotient;
+}
+
+function operate(op, x, y) {
+    return op(x,y)
+}
