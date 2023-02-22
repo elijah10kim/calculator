1b0a0d0a5b1f033edba51835987f94f1b3b94929
Elijah <elijah10kim@gmail.com>
2023-02-21 23:20:45 -0500

    setup basic expression evaluation with equal sign

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
-const screen = document.getElementById('screen');
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+let newScreen = false;
 
 const digitButtons = document.querySelectorAll('.digit-btn');
 digitButtons.forEach((button) => {button.addEventListener('click', () => appendDigit(button.textContent))});
 
+const operatorButtons = document.querySelectorAll('.operator-btn');
+operatorButtons.forEach((button) => {button.addEventListener('click', () => setOperation(button.textContent))})
+
+const screen = document.getElementById('screen');
 const clearButton = document.getElementById('clear-btn');
 const equalButton = document.getElementById('equal-btn');
-const plusButton = document.getElementById('add-btn');
-const minusButton = document.getElementById('subtract-btn');
-const timesButton = document.getElementById('multiply-btn');
-const divideButton = document.getElementById('division-btn');
+const decimalButton = document.getElementById('decimal-btn');
 
-clearButton.addEventListener('click', clearScreen)
+clearButton.addEventListener('click', clearScreen);
+equalButton.addEventListener('click', evaluateExpression);
+
 
 function clearScreen() {
     screen.textContent = 0;
+    num1 = 0;
+    num2 = 0;
+    operator = '';
 }
 
 function appendDigit(digit) {
-    if (screen.textContent === '0') {
+    if (screen.textContent === '0' || newScreen) {
         screen.textContent = digit;
+        console.log(screen.textContent);
+        newScreen = false;
     }
     else {
         screen.textContent += digit;
     }
+}
+
+function setOperation(op) {
+    num1 = Number(screen.textContent);
+    operator = op
+    newScreen = true;
+}
+
+function evaluateExpression() {
+    num2 = Number(screen.textContent);
+    result = Math.round((operate(operator, num1, num2)) * 100) / 100
+    screen.textContent = result;
 }
 
 function add(x,y) {
@@ -53,6 +76,15 @@ function divide(x,y) {
 }
 
 function operate(op, x, y) {
-    return op(x,y);
+    switch(op) {
+        case '+':
+            return add(x,y);
+        case '-':
+            return subtract(x,y);
+        case 'x':
+            return multiply(x,y);
+        case '/':
+            return divide(x,y);
+    }
 }
 
